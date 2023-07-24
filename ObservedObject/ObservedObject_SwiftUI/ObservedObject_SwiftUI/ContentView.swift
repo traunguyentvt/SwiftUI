537915c275dc0f0887a00c6cbe96422a13a494b2
TrauNguyen <nguyenvanngoctrau@gmail.com>
2023-07-23 21:02:34 -0500

    add ObservedObject demo app

---
+++ ObservedObject/ObservedObject_SwiftUI/ObservedObject_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ObservedObject_SwiftUI
+//
+//  Created by VT on 7/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ObservedObject/ObservedObject_SwiftUI/ObservedObject_SwiftUI/ObservedObject_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservedObject_SwiftUIApp.swift
+//  ObservedObject_SwiftUI
+//
+//  Created by VT on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ObservedObject_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
