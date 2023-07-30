c7f4ebb64abfe14417d1743c136a9c278866beae
TrauNguyen <nguyenvanngoctrau@gmail.com>
2023-07-30 11:03:34 -0500

    add simple styles for List view

--- ListView/ListView_SwiftUI/ListView_SwiftUI/Models/Food.swift
+++ ListView/ListView_SwiftUI/ListView_SwiftUI/Models/Food.swift
@@ -5,22 +5,36 @@
 //  Created by VT on 7/27/23.
 //
 
-import Foundation
+import SwiftUI
 
 struct Food: Identifiable, Hashable {
     let id = UUID()
     var name: String
     var image: String
     var isFavorite: Bool
+    var color: Color = .pink
     
     static func createExamples() -> [Food] {
-        [Food(name: "Apple", image: "🍎", isFavorite: true),
-         Food(name: "Banana", image: "🍌", isFavorite: false),
-         Food(name: "Cherry", image: "🍒", isFavorite: true),
-         Food(name: "Mango", image: "🥭", isFavorite: true),
-         Food(name: "Kiwi", image: "🥝", isFavorite: false),
-         Food(name: "Strawberry", image: "🍓", isFavorite: false),
-         Food(name: "Graps", image: "🍇", isFavorite: true)
+        [Food(name: "Apple", image: "🍎", isFavorite: true, color: .pink),
+         Food(name: "Banana", image: "🍌", isFavorite: false, color: .blue),
+         Food(name: "Cherry", image: "🍒", isFavorite: true, color: .yellow),
+         Food(name: "Mango", image: "🥭", isFavorite: true, color: .orange),
+         Food(name: "Kiwi", image: "🥝", isFavorite: false, color: .green),
+         Food(name: "Strawberry", image: "🍓", isFavorite: false, color: .black),
+         Food(name: "Graps", image: "🍇", isFavorite: true, color: .gray)
         ]
     }
+    
+    static func createFoods() -> [Food] {
+        [Food(name: "VT", image: "🍎", isFavorite: true, color: .pink),
+         Food(name: "JV", image: "🍌", isFavorite: false, color: .blue),
+         Food(name: "TVT", image: "🍒", isFavorite: true, color: .green),
+         Food(name: "J", image: "🥭", isFavorite: true, color: .yellow),
+         Food(name: "V", image: "🥝", isFavorite: false, color: .blue),
+         Food(name: "YEVT", image: "🍓", isFavorite: false, color: .blue),
+         Food(name: "AYE-VT", image: "🍇", isFavorite: true, color: .gray)
+        ]
+    }
+    
+    
 }
--- ListView/ListView_SwiftUI/ListView_SwiftUI/Views/FoodCustomCellView.swift
+++ ListView/ListView_SwiftUI/ListView_SwiftUI/Views/FoodCustomCellView.swift
@@ -12,15 +12,78 @@ struct FoodCustomCellView: View {
     @State private var foods: [Food] = Food.createExamples()
     @State private var unhealthyFoods: [Food] = [Food(name: "Pizza", image: "🍕", isFavorite: false),
                                                  Food(name: "Burger", image: "🍔",isFavorite: false)]
+    @State private var vtFoods: [Food] = Food.createFoods()
     
     var body: some View {
         List {
-            ForEach(foods) { food in
-                FoodRow(food: food)
+            Section {
+                ForEach(foods) { food in
+                    FoodRow(food: food)
+                        .listRowSeparatorTint(food.color)
+                        .listRowBackground(Color.yellow)
+                }
+            } header: {
+                Text("Favorites")
+            } footer: {
+                Text("I love JV")
             }
-            ForEach(unhealthyFoods) { food in
-                FoodRow(food: food)
+            .headerProminence(.increased)
+            .foregroundColor(.purple)
+            .font(.body)
+            
+            Section {
+                ForEach(unhealthyFoods) { food in
+                    FoodRow(food: food)
+                }
+//                .listItemTint(.pink)
+                .foregroundColor(.blue)
+                .listRowBackground (
+                    Capsule()
+                        .fill(Color.gray)
+                        .padding(5)
+                )
+            } header: {
+                Text("Loved")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.purple)
+                    .textCase(.uppercase)
+            } footer: {
+                Text("LoveLoveLove")
+                    .font(.body)
             }
+//            .listRowInsets(EdgeInsets.init(top: 20, leading: 50, bottom: 20, trailing: 50))
+            .listRowSeparatorTint(.green, edges: .all)
+            
+            Section {
+                ForEach(vtFoods) { food in
+                    FoodRow(food: food)
+                }
+                .listRowInsets(EdgeInsets.init(top: 0, leading: 40, bottom: 0, trailing: 40))
+                .listRowSeparatorTint(.pink, edges: .top)
+                .listRowSeparator(.hidden)
+            } header: {
+                HStack {
+                    Text("Forever")
+                    Spacer()
+                    Button {
+                        let nfood = Food(name: "JV", image: "🍔", isFavorite: true)
+                        withAnimation {
+                            vtFoods.append(nfood)
+                        }
+                    } label: {
+                        Label("", systemImage: "plus.circle.fill")
+                    }
+                }
+                .foregroundColor(.blue)
+            } footer: {
+                Text("Never forget")
+                    .font(.body)
+                    .bold()
+            }
+            .listSectionSeparatorTint(.blue)
+            .listSectionSeparator(.hidden)
+            
             Button {
                 let nfood = Food(name: "JV", image: "🍔", isFavorite: true)
                 withAnimation {
@@ -28,8 +91,12 @@ struct FoodCustomCellView: View {
                 }
             } label: {
                 Label("VT", systemImage: "plus")
+                    .font(.title)
             }
         }
+        .listStyle(.plain)
+//        .scrollContentBackground(.hidden)
+        .background(Color.yellow.edgesIgnoringSafeArea(.all))
     }
 }
 
